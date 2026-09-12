f563950b883a6187167270c01f69ba501c947a3c
Rafał <rafaloklinski1995@users.noreply.github.com>
2026-09-12 17:48:07 +0200

    Dodaj przycisk powiększania tekstu

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const aktualnyRok = new Date().getFullYear();
 
 document.querySelector("#rok").textContent = aktualnyRok;
+const przyciskTekstu = document.querySelector("#przelacz-tekst");
+
+przyciskTekstu.addEventListener("click", () => {
+  document.body.classList.toggle("wiekszy-tekst");
+});
